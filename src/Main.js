8a0eea5c33954efd412f4008b906aab3517d191a
josh nickson <jnickson1506@gmail.com>
2018-09-22 22:52:41 +0100

    setup react router

--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import Peeps from "./Peeps";
+import About from "./About";
 
 class Main extends Component {
   render() {
     return (
+      <HashRouter>
         <div>
           <h1>Chitter</h1>
           <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/peeps">Peeps</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
           </ul>
           <div className="content">
-
+            <Route exact path="/" component={Home}/>
+            <Route path="/peeps" component={Peeps}/>
+            <Route path="/about" component={About}/>
           </div>
         </div>
+      </HashRouter>
     );
   }
 }
 
 export default Main;
+
